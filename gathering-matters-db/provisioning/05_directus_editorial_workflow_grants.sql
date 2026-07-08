5390902320eafe1dddf7a3823569c1fa0104ab92
Pierce Ohlmeyer-Dawson <ohlmeyerp@gmail.com>
2026-07-08 09:50:27 -0700

    Update directus role grants

---
+++ gathering-matters-db/provisioning/05_directus_editorial_workflow_grants.sql
@@ -0,0 +1,7 @@
+GRANT UPDATE ON TABLE public.content_item TO gm_directus;
+GRANT DELETE ON TABLE public.content_item_tag TO gm_directus;
+GRANT INSERT, DELETE ON TABLE public.content_item_file TO gm_directus;
+GRANT INSERT, UPDATE, DELETE ON TABLE public.content_placement TO gm_directus;
+GRANT INSERT, DELETE ON TABLE public.submission_tag TO gm_directus;
+GRANT INSERT, UPDATE, DELETE ON TABLE public.tag          TO gm_directus;
+GRANT INSERT, UPDATE, DELETE ON TABLE public.content_type TO gm_directus;
